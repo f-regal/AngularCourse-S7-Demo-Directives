abb9745192087f3074202944984a191db87e2079
Faysal Regal <faysalregal@gmail.com>
2022-10-08 20:26:30 +0100

    used prop binding on directives

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,16 +1,31 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2, Input } from '@angular/core';
 
 @Directive({
   selector: '[betterHighlightDirective]'
 })
 export class BetterHighlightDirective {
 
+  @Input() defaultColor: string 
+  @Input() highlightColor: string
+
+  @HostBinding('style.backgroundColor') backgroundColor: string
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
     this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+    this.backgroundColor = this.defaultColor;
   }
 
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue')
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent')
+    this.backgroundColor = this.defaultColor;
+  }
 }
 
 
